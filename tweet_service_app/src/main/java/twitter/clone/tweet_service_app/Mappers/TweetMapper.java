5c181c48f1d68492b2ecbec043eb4fa267e5f109
Sanchit Bansal <sanchitbansal10@gmail.com>
2022-04-03 11:45:17 +0530

    Tweet mapper using mapstruct library to convert database entity data to API response DTO

---
+++ tweet_service_app/src/main/java/twitter/clone/tweet_service_app/Mappers/TweetMapper.java
@@ -0,0 +1,12 @@
+package twitter.clone.tweet_service_app.Mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+import twitter.clone.tweet_service_app.Entity.Tweet;
+import twitter.clone.tweet_service_spec.model.TweetDTO;
+
+@Mapper
+public interface TweetMapper {
+    TweetMapper INSTANCE = Mappers.getMapper( TweetMapper.class );
+    TweetDTO toTweetDTO(Tweet tweet);
+}
