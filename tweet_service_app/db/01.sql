c65a6f0813f81ff42c858b559fdc1f5d880a66a4
Sanchit Bansal <sanchitbansal10@gmail.com>
2022-04-03 11:48:32 +0530

    Docerfile for running tweet app

---
+++ tweet_service_app/db/01.sql
@@ -0,0 +1,10 @@
+CREATE TABLE tweet (
+  id VARCHAR(255) NOT NULL,
+   content VARCHAR(255) NULL,
+   user_id VARCHAR(255) NULL,
+   created_at datetime NULL,
+   updated_at datetime NULL,
+   total_likes INT NULL,
+   total_comments INT NULL,
+   CONSTRAINT pk_tweet PRIMARY KEY (id)
+);
---
+++ tweet_service_app/db/02.sql
@@ -0,0 +1,8 @@
+INSERT INTO tweet (id, content, user_id, created_at, updated_at, total_likes, total_comments)
+VALUES (1, 'Hello World!', 1, '2020-01-01 00:00:00', '2020-01-01 00:00:00', 4, 4);
+
+INSERT INTO tweet (id, content, user_id, created_at, updated_at, total_likes, total_comments)
+VALUES (2, 'Hello World!', 1, '2020-01-01 00:00:00', '2020-01-01 00:00:00', 4, 4);
+
+INSERT INTO tweet (id, content, user_id, created_at, updated_at, total_likes, total_comments)
+VALUES (3, 'Hello World!', 1, '2020-01-01 00:00:00', '2020-01-01 00:00:00', 4, 4);
