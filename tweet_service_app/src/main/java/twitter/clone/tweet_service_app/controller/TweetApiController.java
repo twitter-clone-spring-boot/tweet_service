1fd42038d6ec20d20ec14a0ab2e53dadb436a281
Sanchit Bansal <sanchitbansal10@gmail.com>
2022-04-03 11:45:52 +0530

    Use tweet repository and DTO in get tweet API

--- tweet_service_app/src/main/java/twitter/clone/tweet_service_app/controller/TweetApiController.java
+++ tweet_service_app/src/main/java/twitter/clone/tweet_service_app/controller/TweetApiController.java
@@ -1,12 +1,16 @@
 package twitter.clone.tweet_service_app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.NativeWebRequest;
+import twitter.clone.tweet_service_app.Entity.Tweet;
+import twitter.clone.tweet_service_app.Mappers.TweetMapper;
+import twitter.clone.tweet_service_app.repository.TweetRepository;
 import twitter.clone.tweet_service_spec.api.TweetApi;
-import twitter.clone.tweet_service_spec.model.Tweet;
+import twitter.clone.tweet_service_spec.model.TweetDTO;
 
 import java.util.Optional;
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2022-03-26T12:34:07.575007+05:30[Asia/Kolkata]")
@@ -14,6 +18,9 @@ import java.util.Optional;
 @Controller
 @RequestMapping("${openapi.tweetService.base-path:}")
 public class TweetApiController implements TweetApi {
+
+    @Autowired
+    TweetRepository tweetRepository;
 
     private final NativeWebRequest request;
 
@@ -28,8 +35,14 @@ public class TweetApiController implements TweetApi {
     }
 
     @Override
-    public ResponseEntity tweetTweetIdGet(Integer tweetId) {
-        return new ResponseEntity<Tweet>(HttpStatus.OK);
+    public ResponseEntity tweetTweetIdGet(String tweetId) {
+        Optional<Tweet> tweet = tweetRepository.findById(tweetId);
+        if(tweet.isPresent()) {
+            TweetDTO tweetDTO = TweetMapper.INSTANCE.toTweetDTO(tweet.get());
+            return new ResponseEntity<TweetDTO>(tweetDTO, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<TweetDTO>(HttpStatus.OK);
+        }
     }
 
 }
